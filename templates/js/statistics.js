e62a8490bd3254d46433f7478fa90efcdc3ce85c
Santiago Villagomez <svillagomez@devsu.com>
2018-10-20 16:09:10 -0500

    adding most valuable customer and total average in statistics. TODO most used ingredients

--- templates/js/statistics.js
+++ templates/js/statistics.js
@@ -5,10 +5,32 @@
  * with your service endpoint below
  * ****************************
  */
-fetch('json/statistics.json')
-    .then(response => response.json())
-    .then(statistics => {
-        let template = createStatisticsTemplate(statistics);
+
+fetch('http://localhost:3000/api/create-orders')
+    .then(response => {
+        return response.json()
+    })
+    .then(allOrders => {
+
+        const result = {
+            popular_ingredients: ['pineapple', 'onions'],
+            average_order_total: 18,
+            most_valuable_customer: "Jhon Rambo"
+        };
+        const sortedCustomers = allOrders.sort(compare);
+
+        let totalAverage = 0;
+        allOrders.forEach(order => {
+            console.log(order.total);
+            totalAverage += order.total
+        });
+
+        result.most_valuable_customer = sortedCustomers[0].name;
+        result.average_order_total = (totalAverage/sortedCustomers.length);
+
+        // console.log('sorted Customers', sortedCustomers);
+
+        let template = createStatisticsTemplate(result);
         $("#statistics").append(template);
     });
 
@@ -19,4 +41,13 @@ fetch('json/statistics.json')
 function createStatisticsTemplate(statistics) {
     let template = $("#statistics-template")[0].innerHTML;
     return Mustache.render(template, statistics);
+}
+
+
+function compare(a,b) {
+    if (a.total > b.total)
+        return -1;
+    if (a.total < b.total)
+        return 1;
+    return 0;
 }
